2ea6b302f99423d2a323a78c722bcca61a89ba07
Manoranjan1k <manasj790@gmail.com>
2024-07-01 18:41:45 +0530

    css update

--- WebChat/src/Pages/Room.jsx
+++ WebChat/src/Pages/Room.jsx
@@ -1,16 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react"
 import client, { database , DATABASE_ID, COLLECTION_ID_MESSAGES } from "../appwriteConfig"
 import { ID, Query } from "appwrite";
 import {Trash2} from 'react-feather'
 import Header from '../components/Header'
-
+import { userAuth } from "../utils/AuthContext";
 
 const Room = () => {
-
+    
    const [messages, setMessages] = useState([]);
    const [messageBody, setMessageBody] = useState("");
    
-  
+   
+  const {user} = userAuth()
   useEffect(() => {
     getMessages()
 
@@ -19,6 +21,8 @@ const Room = () => {
        if(response.events.includes("databases.*.collections.*.documents.*.create")){
         console.log('A MESSAGE WAS CREATED')
         setMessages(prevState => [response.payload, ...prevState])
+        
+
        }
 
        if(response.events.includes("databases.*.collections.*.documents.*.delete")){
@@ -36,7 +40,10 @@ const Room = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     let payload = {
+      username:user.name,
+      user_id:user.$id,
       body:messageBody
+      
     }
     
     let response = await database.createDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, 
@@ -51,6 +58,8 @@ const Room = () => {
     // })
 
     setMessageBody('')
+   
+
   }
 
   const getMessages = async () => {
@@ -63,6 +72,7 @@ const Room = () => {
        ]
       )
     setMessages(response.documents)
+
 
   }
 
@@ -97,18 +107,20 @@ const Room = () => {
             <div key={message.$id} className="message--wrapper">
         
             <div className="message--header">
-              <small className="message-timestamp">{new Date(message.$createdAt).toLocaleString()}</small>
+              <small className={message.user_id === user.$id ?"right-side":"message-timestamp"}>{message.username} { new Date(message.$createdAt).toLocaleString()}</small>
               <Trash2 
-                  className="message--btn "
+                  className="message--btn delete--btn"
                   onClick={() => {deleteMessage(message.$id)}}
               />
           
             </div>
-
-              <div className="message--body">
-               <span>
-                  {message.body}
-                </span>
+            
+              <div className="message-box">
+                
+                { message.user_id === user.$id ?<span className="right-side message--body">{message.body}</span>:<span className="left-side message--body--owner">{message.body}</span>}
+                  
+                
+               
               </div>
             </div>
            )
